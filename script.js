a87bbf68f097ad1e8a5f35722f278d0d334bae69
nakaiden <nakaiden@gmail.com>
2022-02-07 21:02:26 -0500

    merge

--- script.js
+++ script.js
@@ -250,3 +250,34 @@ startGameButton.addEventListener('click', () => {
 function mod(n, m) {
     return ((n % m) + m) % m;
 }
+
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+// var x = canvas.width/2;
+// var y = canvas.height-30;
+// var dx = 2;
+// var dy = -2;
+
+// canvas.addEventListener('click',clicked)
+// function clicked(){
+// x = canvas.width/2;
+// y = canvas.height-30;
+// setInterval(draw, 10);
+// }
+// function drawBall() {
+//     ctx.beginPath();
+//     ctx.arc(x, y, 10, 0, Math.PI*2);
+//     ctx.fillStyle = "#0095DD";
+//     ctx.fill();
+//     ctx.closePath();
+// }
+
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     drawBall();
+//   //  x += dx;
+//     y += dy;
+//     dy += .02;
+// }
+
+// drawBall();
