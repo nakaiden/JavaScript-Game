2ab2dc80c1869cf376ea7ed74146805122bf3d6e
nakaiden <nakaiden@gmail.com>
2022-02-12 13:54:23 -0500

    js-update

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const arrowImg = new Image();
 arrowImg.src ='./img/arrow.png';
 
 // get the functions for 2d rendering on our canvas
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 let x = 50;
 let score = 0;
 debug.innerText = `score: ${score}`;
@@ -37,6 +37,8 @@ const FPS = 60;
 class DrawObject{
     x = 50;
     y = 50;
+    dx = 0;
+    dy = 0;
     width = 50;
     height = 100;
     color = 'red';
@@ -45,27 +47,27 @@ class DrawObject{
     draw() {
         this.move();
         //set the draw color of my canvas to my color
-        context.fillStyle = this.color;
-        context.save();
-        context.translate(this.x,this.y);
+        ctx.fillStyle = this.color;
+        ctx.save();
+        ctx.translate(this.x,this.y);
 
         //if this object has rotation, rotate canvas
         if(this.rotation){
-            context.rotate(-this.rotation);
+            ctx.rotate(-this.rotation);
         }
          // draw a rectangle
          if(this.image){
-            context.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);   
+            ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);   
          }
         else if(!this.isCircle){
-            context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+            ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
         } else {
-            context.beginPath();
-            context.arc(-this.width / 2, -this.width / 2, this.width, 0, 2 * Math.PI);
-            context.fill();
-            context.closePath();
+            ctx.beginPath();
+            ctx.arc(-this.width / 2, -this.width / 2, this.width, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
         }
-        context.restore()
+        ctx.restore()
         // this.x += 3;
     }
     move(){
@@ -171,10 +173,32 @@ class Arrow extends DrawObject{
 }
 class Football extends DrawObject{
     image = footballImg;
-    width = 35;
-    height = 55;
+    width = 55;
+    height = 35;
     x = 400;
     y = 550;
+    dy = -5;
+    rotation = Math.PI / 2;
+    speed = 7;
+    
+    move(){
+        this.x += this.dx;
+        this.y += this.dy;
+        this.dy += .02;
+        
+        //check to see if football is colliding w/ goal post(true/false)
+        if(this.isCollding(movingGoal)){
+            console.log('Score!');
+
+        }
+        // is the football on stage
+        
+    }
+
+
+
+    
+  
 }
 const football = new Football();
 const arrow = new Arrow();
@@ -223,7 +247,7 @@ window.addEventListener('keyup', e => {
 
 
 function draw(){
-    context.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     //console.log('mario was drawn');
     //mario.draw();
     //mario2.draw();
@@ -231,13 +255,27 @@ function draw(){
     movingGoal.draw();
     football.draw();
     arrow.draw();
+    //console.log(football.dx,football.dy);
     setTimeout(draw, 1000 / FPS);
 }
+
+
 
 const startGameButton = document.querySelector('.main-menu button');
 const mainMenu = document.querySelector('.main-menu');
 const gameContainer = document.querySelector('.game-container');
 
+canvas.addEventListener('click', () =>{
+    //when we click move football to arrow
+    football.x = arrow.x;
+    football.y = arrow.y;
+    //aim football in same direction as arrow
+    football.rotation = arrow.rotation;
+    //set the footballs velocity based on its rotation
+    football.dx = football.speed * Math.cos(football.rotation)
+    football.dy = football.speed * Math.sin(-football.rotation)
+    
+});
 
 startGameButton.addEventListener('click', () => {
     //hide the main menu
@@ -251,33 +289,3 @@ function mod(n, m) {
     return ((n % m) + m) % m;
 }
 
-// var canvas = document.getElementById("myCanvas");
-// var ctx = canvas.getContext("2d");
-// var x = canvas.width/2;
-// var y = canvas.height-30;
-// var dx = 2;
-// var dy = -2;
-
-// canvas.addEventListener('click',clicked)
-// function clicked(){
-// x = canvas.width/2;
-// y = canvas.height-30;
-// setInterval(draw, 10);
-// }
-// function drawBall() {
-//     ctx.beginPath();
-//     ctx.arc(x, y, 10, 0, Math.PI*2);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-// }
-
-// function draw() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     drawBall();
-//   //  x += dx;
-//     y += dy;
-//     dy += .02;
-// }
-
-// drawBall();
